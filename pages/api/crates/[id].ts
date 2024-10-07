aca4a9051688e252ba18e3c594a056c7ddf0b0cc
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-10-07 16:45:43 +0530

    deploy fix

--- pages/api/crates/[id].ts
+++ pages/api/crates/[id].ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 
 const HTTP_OK = 200;
 const HTTP_NO_CONTENT = 204;
-const HTTP_NOT_FOUND = 404;
+// const HTTP_NOT_FOUND = 404;
 const HTTP_SERVER_ERROR = 500;
 const HTTP_METHOD_NOT_ALLOWED = 405;
 // Initialize the cors middleware
